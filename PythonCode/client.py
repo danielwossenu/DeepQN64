f483b714e3828e84c5f7acbee50d95cdc93427ee
Daniel W <dwossenu6@gmail.com>
2017-05-05 00:52:13 -0400

    changed the client send message to Hi from python

--- PythonCode/client.py
+++ PythonCode/client.py
@@ -5,6 +5,6 @@ host = socket.gethostbyname(socket.gethostname()) # Get local machine name
 port = 12345               # Reserve a port for your service.
 print host
 s.connect(('10.0.0.92', port))
-s.send('exit\n')
+s.send('Hi From Python\n')
 # print s.recv(1024)
 s.close                     # Close the socket when done
